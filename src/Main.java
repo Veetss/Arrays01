cf6a8fcd7fd9b647e9d8c994fc2358a591fb0432
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:50:33 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       String[] carBrands = {"Aston Martin", "Ferrari", "Lamborghini", "Bugatti", "Jaguar"};
+
+        System.out.println("The number of the items is: " + carBrands.length);
+        System.out.println(carBrands[2]);
+
+
+        int[] primeNumbers = new int[6];
+        primeNumbers[0] = 2;
+        primeNumbers[1] = 3;
+        primeNumbers[2] = 5;
+        primeNumbers[3] = 7;
+        primeNumbers[4] = 11;
+        primeNumbers[5] = 13;
+
+        System.out.println(primeNumbers[0]);
+        System.out.println(primeNumbers[1]);
+        System.out.println(primeNumbers[2]);
+        System.out.println(primeNumbers[3]);
+        System.out.println(primeNumbers[4]);
+        System.out.println(primeNumbers[5]);
     }
 }
